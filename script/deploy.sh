13f34e08f08c2c9272d04c20421a37ff01d76538
Nick Houghton <nhoughto5@gmail.com>
2017-06-28 20:01:11 -0700

    Made deploy script bash

--- script/deploy.sh
+++ script/deploy.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 echo "Starting deployment"
 echo "Target: gh-pages branch"
 
@@ -6,7 +6,7 @@ DIST_DIRECTORY="dist/"
 CURRENT_COMMIT=`git rev-parse HEAD`
 ORIGIN_URL=`git config --get remote.origin.url`
 ORIGIN_URL_WITH_CREDENTIALS=${ORIGIN_URL/\/\/github.com/\/\/$GH_TOKEN@github.com}
-
+echo "Parameters set"
 cp .gitignore $DIST_DIRECTORY || exit 1
 
 echo "Checking out gh-pages branch"
